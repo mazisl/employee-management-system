5f617d9359cf30fe06e9501d16691c7e4806ea59
Maz <maz.islam1@gmail.com>
2024-07-12 09:19:36 +0400

    updated landing page cover

--- EMS-Backend/src/index.ts
+++ EMS-Backend/src/index.ts
@@ -2,7 +2,6 @@ import express from 'express';
 import cors from 'cors';
 import { adminRouter } from './routes/AdminRoute';
 import { employeeRouter } from './routes/EmployeeRoute';
-// import jwt from 'jsonwebtoken';
 import cookieParser from 'cookie-parser';
 
 const app = express();
@@ -18,24 +17,6 @@ app.use(cookieParser());
 app.use('/auth', adminRouter);
 app.use('/employee', employeeRouter);
 app.use(express.static('public'));
-
-// const verifyUser = (req, res, next) => {
-//   const token = req.cookies.token;
-//   if (token) {
-//     jwt.verify(token, 'jwt_secret_key', (err, decoded) => {
-//       if (err) return res.json({Status: false, Error: 'Wrong Token'});
-//       req.id = decoded.id;
-//       req.role = decoded.role;
-//       next();
-//     })
-//   } else {
-//     return res.json({Status: false, Error: 'Not Authenticated'})
-//   }
-// }
-
-// app.get('/verify', verifyUser, (req, res) => {
-//   return res.json({Status: true, role: req.role, id: req.id })
-// })
 
 app.listen(3000, () => {
   console.log('Server is running')
--- EMS-Frontend/src/components/start.tsx
+++ EMS-Frontend/src/components/start.tsx
@@ -22,9 +22,13 @@ const Start = () => {
 
   return (
     <div className="loginPage">
-      <div className="loginForm">
+      <div className="loginMain">
+        <h1 className="text-3xl font-bold mb-8 text-center">
+          Welcome to the Employee Management System of 
+          <span className="block w-full text-center">Al Yarmouk Steel!</span>
+        </h1>
         <h2 className="text-2xl font-bold mb-6 text-center">Login As:</h2>
-        <div className="flex justify-between mt-2 mb-1">
+        <div className="flex justify-around mt-2 mb-1">
           <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none" type="button" onClick={() => {navigate('/admin-login')}}>Admin</button>
           <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded focus:outline-none" type="button" onClick={() => {navigate('/employee-login')}}>Employee</button>
         </div>
