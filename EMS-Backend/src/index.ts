ea61a16f039a9166b881d5dabd04917784849b76
Maz <maz.islam1@gmail.com>
2024-08-24 15:33:38 +0400

    heroku deploy attempt 1

--- EMS-Backend/src/index.ts
+++ EMS-Backend/src/index.ts
@@ -21,3 +21,12 @@ app.use(express.static('public'));
 app.listen(3000, () => {
   console.log('Server is running')
 })
+
+//heroku deployment
+import path from 'path';
+
+app.use(express.static(path.join(__dirname, 'client')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'index.html'));
+});
